44492bdb98fbdb98cd17cf6644813ab6b81070d0
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-01-19 20:39:36 +0800

    Add files

---
+++ src-external/Bar.java
@@ -0,0 +1,10 @@
+public class Bar {
+    int value;
+    public Bar(int n) {
+        this.value = n;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
---
+++ src/Foo.java
@@ -0,0 +1,11 @@
+public class Foo {
+    int value;
+
+    public Foo(int n) {
+        this.value = n;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
